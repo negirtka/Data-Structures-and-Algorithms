223ff64e940b28feabc20e42d82d56ecd2edc0f9
negirtka <negiritika2178@gmail.com>
2026-03-07 00:18:45 +0530

    Creating singly, doubly and circular linked list

--- C/LinkedList/Linkedlist.c
+++ C/LinkedList/Linkedlist.c
@@ -1,3 +1,4 @@
+//Creating Singly Linked List
 #include<stdio.h>
 #include<stdlib.h>
 struct node{
@@ -36,5 +37,96 @@ int main(){
         temp=temp->next;
     }
     printf("NULL");
+    return 0;
+}
+
+
+//Creating Doubly linked list
+#include<stdio.h>
+struct node{
+    int data;
+    struct node *prev;
+    struct node *next;
+};
+int main(){
+    struct node *head=NULL;
+    struct node *temp;
+    struct node *newnode;
+    
+    int n,i,;
+    printf("Enter number of Nodes: ");
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++){
+        newnode=(struct node*)malloc(sizeof(struct node));
+        printf("Enter data: ");
+        scanf("%d", &newnode->data);
+        
+        newnode->prev=NULL;
+        newnode->next=NULL;
+        if(head==NULL){
+            head=newnode;
+            temp=newnode;
+        }
+        else{
+            newnode->prev=temp;
+            temp->next=newnode;
+            temp=newnode;   
+        }
+    }
+    printf("Doubly Linked List: ");
+    temp = head;
+
+    while(temp!=NULL){
+        printf("%d <-> ",temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL");
+
+    return 0;
+}
+
+//Creating Circular Linked List
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+int main(){
+    struct node *temp;
+    struct node *head=NULL;
+    struct node *newnode;
+
+    int i,n;
+    printf("Enter number of nodes: ");
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++){
+        newnode=(struct node*)malloc(sizeof(struct node));
+        
+        printf("Enter data:");
+        scanf("%d",&newnode->data);
+        newnode->next=NULL;
+
+        if(head==NULL){
+            head=newnode;
+            temp=head;
+        }
+        else{
+            temp->next=newnode;
+            temp=newnode;
+        }
+    }
+    temp->next=head;    //Making singly linkedlist CIRCULAR
+    printf("Circular LinkedList: ");
+    temp=head;
+    do{
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }while(temp!=head);
+    printf("(Back to head)");
+
     return 0;
 }
